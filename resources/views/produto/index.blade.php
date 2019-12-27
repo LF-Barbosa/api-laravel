b4d02078282e27aebae63b7edfbdaa572d1e9ba7
Luis Felipe Barbosa Teixeira <luisfelipesp2@hotmail.com>
2019-12-27 20:19:23 -0300

    Retorna erro do formulário na index.

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -2,6 +2,15 @@
 @section('title', 'Listagem de produtos')
 @section('content')
 <h1>Produtos</h1>
+    @if(count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+            </ul>
+        </div>
+    @endif
     @if(Session::has('mensagem'))
         <div class="alert alert-success">{{Session::get('mensagem')}}</div>
     @endif
